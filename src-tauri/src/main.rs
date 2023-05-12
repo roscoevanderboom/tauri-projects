56965e0ba1e3afdb5958e4faa1248056aa16b8b8
roscoevanderboom <roscoevanderboom@protonmail.com>
2023-05-12 15:19:36 +0800

    remove file browser

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,13 +1,9 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-mod file_system;
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![
-          file_system::read_dir,
-          file_system::open_file
-      ])
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
